347f326aa9fb4e42291a756df2515e432c13b94c
Keith <keithlang@yahoo.com>
2014-10-20 21:59:34 +0100

    added Google Geocoding request, API, etc - still needs lots of work

--- distance.rb
+++ distance.rb
@@ -1,10 +1,16 @@
+#Keith Lang 2014
+
+require 'net/https'
+require 'uri'
+require 'open-uri'
+
+
 #Distance Between Two Cities - Calculates the distance between
 #two cities and allows the user to specify a unit of distance.
 # This program may require finding coordinates for the cities
 # like latitude and longitude.
 
 # ************
-
 
 # find distance between two cities using co-ordinates from Google
 # Dublin Latitude: 53.3498053
@@ -15,30 +21,63 @@
 
 
 # excel version: ACOS( SIN(lat1)*SIN(lat2) + COS(lat1)*COS(lat2)*COS(lon2-lon1) ) * 6371
-
-
-# javascript version: var x = (λ2-λ1) * Math.cos((φ1+φ2)/2);
-#                     var y = (φ2-φ1);
-#                     var d = Math.sqrt(x*x + y*y) * R;
-
-
-# ask for first latitude (to 2 decimal places)
-puts "enter first latitude to 2 decimal places"
-lat1 = gets
-
-puts "enter second latitude to 2 decimal places"
-lat2 = gets
-
-puts "enter first longtitude to 2 decimal places"
-long1 = gets
-
-puts "enter second longtitude to 2 decimal places"
-long2 = gets
-
-
 # x = Math.acos( Math.sin(53.34)*Math.sin(-34.60) + Math.cos(53.34)*Math.cos(34.60)*Math.cos((-58.38)-(-6.26)) ) * 6371
 #puts x 
 
 
-x = Math.acos( Math.sin(lat1.to_f)*Math.sin(lat2.to_f) + Math.cos(lat1.to_f)*Math.cos(lat2.to_f)*Math.cos(long1.to_f-long2.to_f )) * 6371
-puts x 
+puts "Input first city"
+city1 = gets
+
+puts "Input second city"
+city2 = gets
+
+puts "Calculating......."
+
+# Google Geocoding API over HTTP: http://maps.googleapis.com/maps/api/geocode/output?parameters
+# API key: AIzaSyCFx2ZQEmgkAwbWZHAS8OtpmV1WWRBx2tA
+
+# example of XML query for "1600 Amphitheatre Parkway, Mountain View, CA"
+# https://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCFx2ZQEmgkAwbWZHAS8OtpmV1WWRBx2tA
+
+#JSON version
+# https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCFx2ZQEmgkAwbWZHAS8OtpmV1WWRBx2tA
+
+
+
+
+# Set the request URL
+#url = 'http://api.decibel.net/v1/Albums/?artist=miles%20davis&format=json'
+
+url1 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city1 + '&key=AIzaSyCFx2ZQEmgkAwbWZHAS8OtpmV1WWRBx2tA'
+url2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city2 + '&key=AIzaSyCFx2ZQEmgkAwbWZHAS8OtpmV1WWRBx2tA'
+
+
+
+# Set the request authentication headers
+# timestamp = Time.now.strftime('%Y%m%d %H:%M:%S')
+#headers = {'DecibelAppID' => '<Your Application ID>', 
+#           'DecibelAppKey' => '<Your Application Key>', 
+#           'DecibelTimestamp' => timestamp}
+
+# Send the GET request                              
+# resp = open(url1, headers).read
+resp = open(url1).read
+puts resp
+
+
+#x = Math.acos( Math.sin(lat1.to_f)*Math.sin(lat2.to_f) + Math.cos(lat1.to_f)*Math.cos(lat2.to_f)*Math.cos(long1.to_f-long2.to_f )) * 6371
+#puts x 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
